cb163d438348a97877ace412d96e3038b3968e24
Tor Hellstrand <tor.hellstrand@elev.ga.lbs.se>
2019-01-22 10:49:20 +0100

    HPBasics

---
+++ Assets/Scenes/Scrpits/PlayerHP.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHP : MonoBehaviour
+{
+    
+    public float playerHP = 100;
+    public Scene DeatheScene;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Dead();
+    }
+
+    void Dead()
+    {
+        if(playerHP == 0)
+        {
+            //Load "DeathScene"
+        }
+    }
+
+    void LowHP()
+    {
+        if(playerHP <= 30)
+        {
+            //Något händer som indikerar att man är skadad, Blod på karaktär, pultserande rött runt skrämen .....
+        }
+    }
+}
