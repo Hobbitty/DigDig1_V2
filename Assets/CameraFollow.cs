2acda6ae9350d9f6162185ea71adeb1156f6a614
Andreas Styverbjörne <andreas.styverbjorne@elev.ga.lbs.se>
2019-02-17 14:44:48 +0100

    graphic changes, player can turn, enemy material

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -7,9 +7,22 @@ public class CameraFollow : MonoBehaviour
 
     public Transform playerTank;
 
+    public bool bounds = true;
+
+    public Vector3 minCamPos;
+    public Vector3 maxCamPos;
+
+
     private void FixedUpdate()
     {
         transform.position = new Vector3(playerTank.position.x, playerTank.position.y, -1);
+
+        if (bounds == true)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCamPos.x, maxCamPos.x)
+               , Mathf.Clamp(transform.position.y, minCamPos.y, maxCamPos.y), -1);
+        }
+
     }
 
 }
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -18,6 +18,9 @@ public class PlayerMovement : MonoBehaviour
     [Header("Other")]
     private Rigidbody2D rBody;
     public IsGrounded iG;
+    [Header("Turn Around")]
+    public bool facingLeft;
+    public float lookLeftScale;
 
     private void Start()
     {
@@ -26,8 +29,20 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        moveInput = Input.GetAxis("Horizontal");
+        moveInput = Input.GetAxisRaw("Horizontal");
         rBody.velocity = new Vector2(moveInput * moveSpeed, rBody.velocity.y);
+
+        if (moveInput < 0)
+        {
+            facingLeft = true;
+            transform.localScale = new Vector3(-lookLeftScale, 0.3f);
+        }
+        if(moveInput > 0)
+        {
+            facingLeft = false;
+            transform.localScale = new Vector3(lookLeftScale, 0.3f);
+        }
+
 
         if (Input.GetKeyDown(KeyCode.Space) && iG.isGrounded == true)
         {
@@ -45,9 +60,8 @@ public class PlayerMovement : MonoBehaviour
             else
                 isJumping = false;
         }
+
         if (Input.GetKeyUp(KeyCode.Space))
             isJumping = false;
-
-
     }
 }
