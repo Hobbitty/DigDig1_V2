2ea8cf3453132bde8e81132097da53e8e85e9d34
leo lundberg <leo.lundberg@elev.ga.lbs.se>
2019-03-19 09:50:54 +0100

    level 1

--- Assets/Leo Scripts/Scripts/PlayerAttack.cs
+++ Assets/Leo Scripts/Scripts/PlayerAttack.cs
@@ -41,7 +41,7 @@ public class PlayerAttack : MonoBehaviour
         CanAttackDown();
         Attacking();
         AttackingCooldown();
-        //RangedAttack();
+        RangedAttack();
     }
 
     void Attacking()
