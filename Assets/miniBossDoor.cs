1d7249cc5e5a983bb0a2e01903f2c7a80d1292b0
leo lundberg <leo.lundberg@elev.ga.lbs.se>
2019-03-25 08:21:28 +0100

    lvl2 och lvl 1 ändringar

---
+++ Assets/miniBossDoor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class miniBossDoor : MonoBehaviour
+{
+
+    public GameObject miniBoss;
+    public bool miniBossAlive;
+    public BoxCollider2D door;
+    public string sceneToLoad;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+       
+
+        if (miniBoss.active == true)
+        {
+            miniBossAlive = true;
+        }
+            
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "player" && miniBossAlive == false)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
+
+    
+
