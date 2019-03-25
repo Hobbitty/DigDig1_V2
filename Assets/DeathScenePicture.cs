6bcad740a25c690646242c7933c4abaa8d5aa63f
Andreas Styverbjörne <andreas.styverbjorne@elev.ga.lbs.se>
2019-03-25 10:46:53 +0100

    level before sloth boss death scene animation

---
+++ Assets/DeathScenePicture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScenePicture : MonoBehaviour
+{
+    public float travelSpeed;
+    public Vector2 startPose;
+    public Vector2 endPose;
+
+    public Rigidbody2D rbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        transform.position = startPose;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbody.AddForce(transform.forward * travelSpeed* Time.deltaTime);
+    }
+}
