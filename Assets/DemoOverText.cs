cfef447f2394d749af178dff006c0cd40ce6ed63
Andreas Styverbjörne <andreas.styverbjorne@elev.ga.lbs.se>
2019-04-29 08:22:23 +0200

    nothing changed

---
+++ Assets/DemoOverText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DemoOverText : MonoBehaviour
+{
+    public GameObject DemoOverCanvas;
+
+    public float timer = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject theBoss = GameObject.FindWithTag("Boss");
+        if (theBoss != null)
+        {
+            EnemyHealth bossHP = theBoss.GetComponent<EnemyHealth>();
+            {
+                if (bossHP != null)
+                {
+                    if (bossHP.healthPoints <= 0)
+                        DemoOverCanvas.SetActive(true);
+                    else
+                        DemoOverCanvas.SetActive(false);
+                }
+            }
+        }
+
+
+        if (DemoOverCanvas.activeSelf == true)
+            timer -= Time.deltaTime;
+
+        if (timer <= 0)
+            SceneManager.LoadScene(0);
+    }
+}
