4568435eb81e331b86b84675402ed29adf27c855
Hobbitty <Linus.skorupa@elev.ga.lbs.se>
2019-05-07 08:50:35 +0200

    Linus Projekt från gammal branch

---
+++ Assets/Scripts(new)/ButtonScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Vector3 realSize;
+    public Vector3 pressSize;
+    [Header("Keep Clear")]
+    public SpriteRenderer rend;
+    private CheckpointUnlock cU;
+
+    private void Start()
+    {
+        rend = gameObject.GetComponent<SpriteRenderer>();
+        realSize = transform.localScale;
+        cU = gameObject.GetComponentInParent<CheckpointUnlock>();
+    }
+
+    private void Update()
+    {
+        if (cU.inRange == false)
+        {
+            Destroy(gameObject);
+        }
+        //if (cU.inRange == true && Input.GetKeyDown(KeyCode.E))
+        //{
+        //    transform.localScale = pressSize;
+        //    rend.color = new Color(0.5f, 0.5f, 0.5f);
+        //}
+        //if (Input.GetKeyUp(KeyCode.E))
+        //{
+        //    rend.color = new Color(1, 1, 1);
+        //    transform.localScale = realSize;
+        //}
+
+    }
+}
---
+++ Assets/Scripts(new)/CheckpointManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManagerScript : MonoBehaviour
+{
+    private static CheckpointManagerScript Instance;
+    public Vector3 lastCheckPos;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+        else
+            Destroy(gameObject);
+
+    }
+}
