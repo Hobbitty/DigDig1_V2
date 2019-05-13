92e40aa820e80d91b024630626e74988363bd259
leo lundberg <leo.lundberg@elev.ga.lbs.se>
2019-05-13 10:01:36 +0200

    no message

--- Assets/destroy.cs
+++ Assets/destroy.cs
@@ -7,12 +7,39 @@ public class destroy : MonoBehaviour
     public int timer;
     public static bool playerPickedUpAmmo;
     public AudioSource reload;
+    private Rigidbody2D rBody;
+    public bool bossFight;
+    public float bossTImer;
+    public int fall;
+
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.bodyType = RigidbodyType2D.Static;
+        rBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
 
     void Update()
     {
        if(timer >= 1)
         {
             Destroy(gameObject);
+        }
+
+        if (bossFight == true)
+        {
+            bossTImer = bossTImer + 1 * Time.deltaTime;
+
+            if(bossTImer >= 30)
+            {
+                fall = Random.Range(1, 1000);
+            }
+
+            if(fall == 50)
+            {
+                fall = 50;
+                rBody.bodyType = RigidbodyType2D.Dynamic;
+            }
         }
     }
 
